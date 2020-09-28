9b3694b34837f6f1176e29bdb29293f36a192530
Rohith Reddy Mandala <reddy.rohith09@gmail.com>
2020-09-28 10:57:44 -0700

    Initial Changes
    OCR implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import * as Tesseract from 'tesseract.js';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-ocr';
+  fileToUpload: File = null;
+
+  handleFileInput(files: FileList) {
+    this.fileToUpload = files.item(0);
+  }
+  imageOcr() {
+    Tesseract.recognize(this.fileToUpload)
+      .then((res: any) => {
+        const text = res.data.text.split(/\r?\n/);
+        const newText = text[5].split(' ');
+        const result = newText.filter((word) => word.length > 16);
+        console.log(result);
+      })
+      .catch(console.error);
+  }
 }
